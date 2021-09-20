665176ba7ae6783b182ce03a5be525d6148bbedf
Rithik Sachdev <39690921+rithiksachdev@users.noreply.github.com>
2021-09-20 17:59:11 +0530

    Adding comments and some bug fixes

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ var content=[]
 var count = 0
 time = []
     
-//alert("sdvs")
-
+//alert("")
+/*
+    This function is used to add new note headings in the unordered list and update the heading
+*/
 function newNote(){
-    //alert("dvsdv");
+    //alert("");
     var li = document.createElement("li");
     var inputHeading = document.getElementById("leftHeading").value;
     var temp =document.createTextNode(inputHeading);
@@ -32,6 +34,11 @@ function newNote(){
     }
 }
 
+/*
+    This function is used to add save note content for a corresponding heading in the unordered 
+    list and update the time.Here I have used heading as the key as it cannot be edited in label, 
+    but we can use long integers if we want to support same heading notes.
+*/
 function saveNote(){
         var temp = document.getElementById("leftHeadingNotes").innerHTML;
         for (j = 0; j<headings.length; j++)
@@ -48,6 +55,7 @@ function saveNote(){
         }
 }
 
+// Deletes a note based on the heading
 function deleteNote()
 {
     var temp = document.getElementById("leftHeadingNotes").innerHTML;
@@ -58,11 +66,11 @@ function deleteNote()
         {
             var list = document.querySelector('ul');
             list.removeChild(document.getElementById(j))
-            document.getElementById("leftHeadingNotes").innerHTML=headings[0];
+            document.getElementById("leftHeadingNotes").innerHTML="Notes Heading";
         
-    	document.getElementById("leftHeadingNotesContent").value=content[0];   				    	
+    	document.getElementById("leftHeadingNotesContent").value="";   				    	
         	
-        document.getElementById("rightHeadingNotes").innerHTML = time[0];
+        document.getElementById("rightHeadingNotes").innerHTML = "Time";
         }
     }
 
